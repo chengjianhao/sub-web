02cd4a56d4175dbfe0f0a5ca30842048ca07ef97
Mio <39869364+chengjianhao@users.noreply.github.com>
2021-05-25 14:54:00 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -268,7 +268,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          "subcc.vercel.app (自动编译最新版本后端-测试）": "https://sub-beta.now.sh/sub?",
+          "subcc.vercel.app (vercel服务）": "https://sub-beta.now.sh/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
